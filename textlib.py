4ebb13792b6c94a25f5e68086b9e5a5420d07d97
Joe Dewey <jndewey@gmail.com>
2019-06-17 04:55:00 -0400

    updated model deployment

--- textlib.py
+++ textlib.py
@@ -2,29 +2,25 @@ import csv
 import os
 import os.path
 import re
-
+import html2text
 import nltk as nltk
-#import textract
+import textract
 from bs4 import BeautifulSoup as BeautifulSoup
 from openpyxl import Workbook as Workbook
 
 
-def save_list_as_excel(path, clean_sent_list, raw_sent_list):
-    wb = Workbook()
-    ws = wb.active
-    column_cell_A = 'A'
-    column_cell_B = 'B'
-    column_cell_C = 'C'
-    ws[column_cell_A + str(1)] = 'text'
-    ws[column_cell_B + str(1)] = 'label'
-    ws[column_cell_C + str(1)] = 'raw'
-    list_len = len(clean_sent_list)
-    for i in range(0, list_len):
-        ws[column_cell_A + str(i + 2)] = clean_sent_list[i]
-        ws[column_cell_B + str(i + 2)] = 'label'
-        ws[column_cell_C + str(i + 2)] = raw_sent_list[i]
-    wb.save(path)
-    print('Excel write complete')
+def load(path):
+    if path.endswith('html'):
+        return html_from_file_no_tags(path)
+    elif path.endswith('.txt'):
+        return str(text_from_file(path))
+    else:
+        try:
+            return str(text_from_binary(path).decode('utf-8'))
+        except:
+            print(
+                'Failed to load as binary. Try reader that accepts url as argument (e.g., html_from_web_tags(url) or '
+                'html_from_web_no_tags(url)).')
 
 
 def html_from_file_no_tags(file_path):
@@ -34,47 +30,26 @@ def html_from_file_no_tags(file_path):
     return bsObj
 
 
-def normalize_sent(sent):
+def normalize_sent(sent, lower=True):
     wpt = nltk.WordPunctTokenizer()
     stop_words = nltk.corpus.stopwords.words('english')
     sent = re.sub(r'[^a-zA-Z\s]', '', sent, re.I | re.A)
     sent = sent.strip()
     tokens = wpt.tokenize(sent)
     filtered_tokens = [token for token in tokens if token not in stop_words]
-    sent = ' '.join(filtered_tokens).lower()
+    if lower:
+        sent = ' '.join(filtered_tokens).lower()
+    else:
+        sent = ' '.join(filtered_tokens)
     return sent
 
 
-def html_from_file_no_tags(file_path):
-    with open(file_path, 'rb') as myfile:
-        raw_text = myfile.read()
-    bsObj = BeautifulSoup(raw_text, 'lxml').text
-    return bsObj
-
-
-def load_normal_with_stopwords(path):
-    doc = str(load_raw(path))
-    return normalize_document_return_list(doc)
-
-
-def load_normal_no_stopwords(path):
-    doc = load_raw(path)
-    norm = normalize_document_return_list(doc)
-    clean_sent = []
-    for sent in norm:
-        clean = remove_stop_words(sent)
-        clean_sent.append(clean)
-    return clean_sent
-
-
-def list_from_directory(path):
-    list_of_text = []
+def load_from_dir(path):
     for file in os.listdir(path):
         filename = os.fsdecode(file)
         file_path = path + filename
-        text = load_raw(file_path)
-        text = str(text)
-        list_of_text.append(text)
+        text = load(file_path)
+        yield text
 
 
 def load_excel(path):
@@ -96,9 +71,9 @@ def load_list_from_csv(path):
         return list_clean_two
 
 
-def load_raw(path, tags=False):
+def load(path, tags=False):
     if path.endswith('html'):
-        if (tags):
+        if tags:
             return html_from_file_tags(path)
         else:
             return html_from_file_no_tags(path)
@@ -109,13 +84,14 @@ def load_raw(path, tags=False):
             return str(text_from_binary(path))
         except:
             print(
-                'Failed to load as binary. Try reader that accepts url as argument (e.g., html_from_web_tags(url) or html_from_web_no_tags(url)).')
+                'Failed to load as binary. Try reader that accepts url as argument (e.g., html_from_web_tags(url) or '
+                'html_from_web_no_tags(url)).')
 
 
 def text_from_binary(file_path):
-    #text = textract.process(file_path, method='tesseract', language='eng')
-    text = 'Not set up for binary'
-    return text.decode('unicode_escape').encode('utf-8', 'ignore').strip()
+    text = textract.process(file_path, method='tesseract', language='eng')
+    #return text.decode('unicode_escape').encode('utf-8', 'ignore').strip()
+    return text.decode('utf-8')
 
 
 def html_from_file_tags(file_path):
@@ -177,3 +153,14 @@ def tokenize_and_stem(text):
     joined_text = " ".join(stems)
     print(joined_text)
     return joined_text
+def low(object):
+    print(object.lower())
+x = load_from_dir('/Users/josiasdewey/Downloads/credit/')
+for i in x:
+    print(i)
+
+
+
+
+
+
